b5ef7468c0ca688bd38a196a31fe694875e590d9
laihz <2190732276@qq.com>
2024-09-27 14:12:45 +0800

    part 1

--- main.c
+++ main.c
@@ -5,28 +5,67 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
-typedef struct {
-    char* buffer;
+typedef struct
+{
+    char *buffer;
     size_t buffer_length;
     ssize_t input_length;
 
 } InputBuffer;
 
-InputBuffer* initBuffer(){
-    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+InputBuffer *initBuffer()
+{
+    InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
     input_buffer->buffer = NULL;
     input_buffer->buffer_length = 0;
     input_buffer->input_length = 0;
     return input_buffer;
-
 }
+
+void print_prompt()
+{
+    printf("db > ");
+}
+
+//ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 int main(int argc, char const *argv[])
 {
+    InputBuffer *input_buffer = initBuffer();
     while (1)
-    {
+    {   
+        print_prompt();
+        read_input(input_buffer);
+        if (strcmp(input_buffer->buffer,".exit") == 0)
+        {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        }else{
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
         
+
     }
     return 0;
+}
+
+void read_input(InputBuffer *input_buffer)
+{
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    if (bytes_read < 0)
+    {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+    //忽略换行符
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+    
+
+}
+
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
 }
